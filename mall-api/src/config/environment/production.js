804909111384041c4998220f3706232c0f574f5e
xyl1993 <602165057@qq.com>
2018-11-11 12:56:16 +0800

    Update mall-api/src/config/environment/production.js

--- mall-api/src/config/environment/production.js
+++ mall-api/src/config/environment/production.js
@@ -2,12 +2,12 @@ module.exports = {
   mysql: {
     host: 'localhost',
     user: 'root',
-    password: 'XylAliyun110!',
+    password: 'szjcy@sql',
     database: 'mall',
     port: 3306
   },
   jwtEncryption:
     'a473098d763c06377289597a21a7e71e3584a1168e54a6ef3d41902cea3d92c248d1abdb4636f640bb502f3b7f58cc0fdaaacda61623f47a887bf148b6101f27',
   port: 8002,
-  filePath:'http://47.98.243.170/mall/upload/'
+  filePath:'https://www.billionsen.cn/mall/upload/'
 };
